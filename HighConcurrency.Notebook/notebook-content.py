2b6e98b88c8924ba50a8550aedc4f9154047cd1a
HashiruNewland <45444193+HashiruNewland@users.noreply.github.com>
2026-04-13 21:22:43 -0400

    Committing 3 items from workspace cbfd01ac-d3b6-4081-bcbb-7920774a3fd8

--- HighConcurrency.Notebook/notebook-content.py
+++ HighConcurrency.Notebook/notebook-content.py
@@ -13,7 +13,51 @@
 
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
+%conda install altair          # install latest version through conda command
+%conda install vega_datasets   # install latest version through conda command
 
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+import altair as alt
+from vega_datasets import data
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# load a simple dataset as a pandas DataFrame
+cars = data.cars()
+alt.Chart(cars).mark_point().encode(
+x='Horsepower',
+y='Miles_per_Gallon',
+color='Origin',
+).interactive()
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+#Manage Python custom libraries through inline installation
+
+# install the .whl through pip command from the notebook built-in folder
+%pip install "builtin/wheel_file_name.whl"
 
 # METADATA ********************
 
